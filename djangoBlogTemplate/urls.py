dd6107423c8d9b9fee2d3af2f1a94c92644dad38
Anup Bhattarai <anup.bhattarai.ace@gmail.com>
2022-01-17 09:35:58 +0545

    Cleaned the Blog

--- djangoBlogTemplate/urls.py
+++ djangoBlogTemplate/urls.py
@@ -1,4 +1,4 @@
-"""iCoder URL Configuration
+"""djangoBlogTemplate URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- djangoBlogTemplate/wsgi.py
+++ djangoBlogTemplate/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for iCoder project.
+WSGI config for djangoBlogTemplate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iCoder.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoBlogTemplate.settings")
 
 application = get_wsgi_application()
